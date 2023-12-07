1766206c14f82166ab2ca5c4b1165e8e38088ec0
Sam <sam.cooke@education.gov.uk>
2023-12-07 12:22:47 +0000

    2023 day 6 part 1

--- 2023/day5.R
+++ 2023/day5.R
@@ -52,3 +52,26 @@ day5_1 <- function(input) {
 
 #answer
 day5_1(input)
+
+
+day5_2 <- function(input) {
+  data <- split(input, cumsum(input == ""))
+  seeds <- str_extract_all(data[[1]], "\\d+ \\d+")[[1]]
+  
+  seed_pairs <- lapply(seeds, \(x) as.numeric(str_split(x, " ")[[1]]))
+  instr <- data[2:length(data)]
+  answer <- vector(mode = "numeric")
+  
+  for(seed in unlist(lapply(seed_pairs,\(x) seq(x[1], sum(x))))) {
+    output <- c(seed)
+    
+    for (i in seq(1, length(instr))) {
+      output[length(output) + 1] <- checker(output[length(output)], parser(instr[i]))
+    }
+    answer[length(answer) + 1] <- output[length(output)]
+  }
+  min(answer)
+}
+
+day5_2(input)
+
