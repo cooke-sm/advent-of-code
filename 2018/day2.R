66285f9b87fbd8c5cf55e69b25e544f77ebd5b9f
COOKE <Sam.COOKE@EDUCATION.GOV.UK>
2025-11-14 16:09:14 +0000

    2018 day 2

--- 2018/day2.R
+++ 2018/day2.R
@@ -1,5 +1,26 @@
 input <- readLines("~/advent-of-code/2018/data/day2_input.txt")
 
+comparer <- function(x,y){
+  difs <- 0
+
+  for(letter in seq(1,length(x))){
+    
+    if((x[letter] == y[letter]) & (difs < 2)){
+      
+    } else if(difs >1){
+      break
+    } else{
+      difs <- difs +1
+    }
+        
+  }
+
+  if(difs == 1){
+    intersect(x, y)
+  } else {}
+}
+
+
 day2_1 <- function(input){
 
   codes <- strsplit(input, "")
@@ -20,4 +41,19 @@ day2_1 <- function(input){
 
 day2_1(input)
 
+day2_2 <- function(input){
+  codes <- strsplit(input, "")
 
+  for(code in seq(1, length(codes))){
+
+    matches <- unlist(lapply(codes, \(x) comparer(codes[[code]], x)))
+    
+    if(length(matches) > 0){
+      print((paste(matches, collapse = "")))
+      print(codes[[code]])
+    }
+  }
+
+}
+
+day2_2(input = input)
